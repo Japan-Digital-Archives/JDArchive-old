b602b3f7d09839654a254de70a782ad0e9d80c95
Arne Brasseur <arne@arnebrasseur.net>
2011-07-03 16:02:50 +0200

    add extra columns for location name, client ip, timestamp, spam score (future use) and bitmask flags (future use)

---
+++ mvc/sql/002-extra-testimonial-columns.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `testimonial`
+      ADD COLUMN created TIMESTAMP NOT NULL DEFAULT NOW(),
+      ADD COLUMN spamkarma FLOAT DEFAULT 0.0,
+      ADD COLUMN client_ip VARCHAR(15),
+      ADD COLUMN flags TINYINT DEFAULT 1;
+
+ALTER TABLE `testimonial_location` ADD COLUMN `name` VARCHAR(255);

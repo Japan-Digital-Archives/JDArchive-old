f408284daced03e3e6ad86d91b8cff5951bcaf0d
Arne Brasseur <arne@arnebrasseur.net>
2011-10-06 09:29:43 -0400

    Fix SQL (for real now) and small python bugfix

--- python/tableCommands.sql
+++ python/tableCommands.sql
@@ -1,11 +1,11 @@
 SET NAMES 'utf8';
 
-DROP TABLE IF NOT EXISTS `tweets`;
+DROP TABLE IF EXISTS `tweets`;
 CREATE TABLE `tweets` (
   `tweetId` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `userId` varchar(50) NOT NULL,
   `createdAt` datetime NOT NULL,
   `twitterId` varchar(75) NOT NULL,
   `geoInfo` varchar(500) DEFAULT NULL,
-  `Text` varchar(500) NOT NULL,
+  `Text` varchar(500) NOT NULL
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;

4074faee00b714c36870c16957eb1865e20cfea4
Arne Brasseur <arne@arnebrasseur.net>
2011-10-06 15:10:03 +0200

    Code review Twitter script : fix SQL and configure DB

--- python/tableCommands.sql
+++ python/tableCommands.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tweets (tweetId INT,userId VARCHAR(50), createdAt DATETIME, twitterId VARCHAR(75), geoInfo NVARCHAR(500), Text NVARCHAR(1000));
-ALTERTABLE tweets ADD PRIMARY KEY (tweetId);
+ALTER TABLE tweets ADD PRIMARY KEY (tweetId);
 ALTER TABLE tweets ADD INDEX(tweetId);
 ALTER TABLE tweets MODIFY userId VARCHAR(50) NOT NULL;
 ALTER TABLE tweets MODIFY createdAt DATETIME NOT NULL;
@@ -7,7 +7,7 @@ ALTER TABLE tweets MODIFY twitterId VARCHAR(75) NOT NULL;
 ALTER TABLE tweets MODIFY Text VARCHAR(500) NOT NULL;
 ALTER TABLE tweets MODIFY COLUMN tweetId INT NOT NULL AUTO_INCREMENT;
 
---insert these two lines into /etc/mysql/my.cnf under the [mysqld] section
---default-character-set=utf8
---skip-character-set-client-handshake
---Tue, 04 Oct 2011 02:51:27 +0000
+-- insert these two lines into /etc/mysql/my.cnf under the [mysqld] section
+-- default-character-set=utf8
+-- skip-character-set-client-handshake
+-- Tue, 04 Oct 2011 02:51:27 +0000

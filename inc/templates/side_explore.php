6cb494e014132cace0f1ef76dbd61eba46120d6e
Julie Zhang <blackdragon7170@gmail.com>
2011-10-30 13:16:11 -0400

    whoops. Actually added the files this time

---
+++ inc/templates/side_explore.php
@@ -0,0 +1,21 @@
+      <div id="sidebar">
+        <ul>
+          <li>
+            <h2><a href="../seeds/">Search the Web Archive</a></h2>
+                <p>Search the collection of harvested websites and website information.</p>
+          </li>
+          <li>
+              <h2><a href="../contribute/">Contribute</a></h2>
+              <p>Submit websites related to the disasters through our <a href="../contribute/">form</a> or by using our bookmarklet: <a class="bookmarklet" href="javascript:q=location.href;if(document.getSelection){d=document.getSelection();}else{d='';};p=document.title;void(open('http://jedarchive.org/bookmarklet?internal=true&url='+encodeURIComponent(q)+'&description='+encodeURIComponent(d)+'&title='+encodeURIComponent(p),'Pinboard',%20'toolbar=no,width=660,height=450'));">JDArchive</a> Next time you're on a website you think is useful, just click the bookmark and we'll auto-fill some of the fields.</p>
+          </li>
+          <li>
+              <h2><a href="../featured/">Featured Testimonials</a></h2>
+              <p>Visit our featured testimonial page to read some of the contributions to the testimonial collection.</p>
+          </li>
+          <li>
+              <h2><a href="http://worldmap.harvard.edu/japanmap/">Browse our Map Layers</a></h2>
+              <p>Explore rich map layers that can allow anyone to browse some of the many rich data sets that have been shared with the Japan Sendai Earthquake Data Portal.</p>
+          </li>
+        </ul>
+      </div>
+      <!-- end #sidebar -->

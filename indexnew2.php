bb4d77f24be89a63cc188939c9ffabe0910c36a5
Julie Zhang <blackdragon7170@gmail.com>
2011-11-04 15:10:45 -0400

    changes round2

---
+++ indexnew2.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once(dirname(__FILE__). '/inc/common.php');
+require_once(dirname(__FILE__). '/inc/templates/head.php');
+
+//$language = language();
+
+?>
+<link href="/lib/launch2.css" rel="stylesheet" type="text/css" media="screen" />
+
+<div id="outerbox">
+
+<div id="homebuttonblack">
+	<a href="../explore/">
+	<div class="homelogoleft">
+	   <img src="lib/images/logo120.png">
+	</div>
+	
+	<div class="buttontextright">
+		<h2 style="color:white;">Explore the<br /> Archive Now</h2>
+	</div>
+	</a>
+</div>
+
+
+<div id="homebuttonwhite">
+	<a href="../new/">
+	<div class="buttontextleft">
+	 <h2 style="color:2E2E2E;">これからの<br/>アーカイブ</h2>
+	</div>
+	<div class="homelogoright">
+	   <img src="lib/images/logo120.png">
+	</div>
+	</a>
+</div>
+
+<div id="homebuttonwhite">
+	<a href="../new/">
+	<div class="buttontextleft">
+	 <h2 style="color:2E2E2E;">The Archive<br /> We Are Building
+	</div>
+	<div class="homelogoright">
+	   <img src="lib/images/logo120.png">
+	</div>
+	</a>
+</div>
+
+
+<div id="homebuttonblack">
+	<a href="../explore/">
+	<div class="homelogoleft">
+	   <img src="lib/images/logo120.png">
+	</div>
+	
+	<div class="buttontextright">
+		<h2 style="color:white;">ここまで<br />の<br/>アーカイブ</h2>
+	</div>
+	</a>
+</div>
+</div>
+
+<div id="footer">
+ <div id="footer">
+    <div class="footerleft">
+    <img src="lib/images/logo-trans.png">
+    </div>
+    <p><a href="http://www.fas.harvard.edu/~rijs/"><img src="/lib/images/reisch_logo.png" style="margin-top:2px; border:none;" /></a></p>
+</div>
+</div>
+
+

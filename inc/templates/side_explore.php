5c3a65c36574306c496ec50b42dde4bf979ff4ef
K. M. Lawson <raven@muninn.net>
2011-11-07 15:04:14 -0500

    Launch page modifications and misc.

--- inc/templates/side_explore.php
+++ inc/templates/side_explore.php
@@ -1,22 +1,52 @@
+	 <?php $language = language(); ?>
+
+	<?php if ($language == 'ja'): ?>
       <div id="sidebar">
+        <h2 >ここまでのアーカイブ</h2></h2>
         <ul>
           <li>
-            <h2><a href="../seeds/">Search the Web Archive</a></h2>
+
+            <h2><a href="../seeds/">ウェブ・アーカイブを検索する</a></h2>
+                <p>当アーアイブに収蔵されたウェブサイトやそれらについての情報、例えばページのタイトルや説明、関連するタグなどを検索することができます。</p>
+          </li>
+          <li>
+              <h2><a href="../contribute/">ウェブサイトの投稿</a></h2>
+              <p>アーカイブに適うと皆様が考える震災関連のウェブサイトは収蔵されていましたか？もしそうでなければ、投稿用のページかまたはJDArchiveのブックマークをお使いになり投稿にご協力下さい。</p>
+          </li>
+          <li>
+              <h2><a href="../featured/">わたしの「東日本大震災」</a></h2>
+              <p>これまでに皆様から寄せられた「わたしの『東日本大震災』」から、いつくかの投稿をご紹介しております。「わたしの『東日本大震災』」ページを直接ご利用になることで皆様の<a href="../testimonial/">体験を共有しませんか。</a></p>
+          </li>
+          <li>
+              <h2><a href="http://worldmap.harvard.edu/japanmap/">地理空間情報レイヤー</a></h2>
+              <p>地理学分析センターと恊働することで、私たちがご提供する地理空間情報レイヤーは深化し続けています。このレイヤー情報により、同センターが制作したJapan Sendai Earthquake Data Portalと共有している豊富なデータをどなたでもご覧頂くことができます。</p>
+          </li>
+        </ul>
+      </div>
+      <!-- end #sidebar -->
+      <?php else: ?>
+      <div id="sidebar">
+        <h2>Accessible Now</h2></h2>
+        <ul>
+          <li>
+
+            <h2><a href="../seeds/?la=en">Search the Web Archive</a></h2>
                 <p>Search the collection of harvested websites and website information.</p>
           </li>
           <li>
-              <h2><a href="../contribute/">Contribute</a></h2>
-              <p>Submit websites related to the disasters through our <a href="../contribute/">form</a> or by using our bookmarklet: <a class="bookmarklet" href="javascript:q=location.href;if(document.getSelection){d=document.getSelection();}else{d='';};p=document.title;void(open('http://jedarchive.org/bookmarklet?internal=true&url='+encodeURIComponent(q)+'&description='+encodeURIComponent(d)+'&title='+encodeURIComponent(p),'Pinboard',%20'toolbar=no,width=660,height=450'));">JDArchive</a> Next time you're on a website you think is useful, just click the bookmark and we'll auto-fill some of the fields.</p>
+              <h2><a href="../contribute/?la=en">Contribute</a></h2>
+              <p>Submit websites related to the disasters through our <a href="../contribute/">form</a> or by using our bookmarklet. Next time you're on a website you think is useful, just click the bookmark and we'll auto-fill some of the fields.</p>
           </li>
           <li>
-              <h2><a href="../featured/">Featured Testimonials</a></h2>
+              <h2><a href="../featured/?la=en">Featured Testimonials</a></h2>
               <p>Visit our featured testimonial page to read some of the contributions to the testimonial collection or submit your own story through our 
               <a href="../testimonial/">form</a>.</p>
           </li>
           <li>
-              <h2><a href="http://worldmap.harvard.edu/japanmap/">Browse our Map Layers</a></h2>
+              <h2><a href="http://worldmap.harvard.edu/japanmap/?la=en">Browse our Map Layers</a></h2>
               <p>Explore rich map layers that can allow anyone to browse some of the many rich data sets that have been shared with the Japan Sendai Earthquake Data Portal.</p>
           </li>
         </ul>
       </div>
       <!-- end #sidebar -->
+       <?php endif ?>
--- inc/templates/stop_base.php
+++ inc/templates/stop_base.php
@@ -5,8 +5,14 @@
 </div>
 <div id="footer-bgcontent">
   <div id="footer">
-    <p><a href="http://www.fas.harvard.edu/~rijs/"><img src="/lib/images/reisch_logo.png" style="margin-top:2px;" /></a></p>
-  </div>
+    <div class="footerleft">
+    <a href="http://twitter.com/#!/jdarchive"><img style="padding-top:4px" src="/lib/images/t.png"></a> <br />
+    <a href="https://www.facebook.com/pages/Digital-Archive-of-Japans-2011-Disasters/143040332434759"><img src="/lib/images/f.png"></a>
+    </div>
+    <p><a href="http://www.fas.harvard.edu/~rijs/"><img src="/lib/images/reisch_logo.png" style="margin-top:2px; border:none;" /></a></p>
+</div>
+
+
 </div>
 <!-- end #footer -->
 </body>

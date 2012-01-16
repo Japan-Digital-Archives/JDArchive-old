1f7e0c55497634dcb7466b0c99e765a3473a8dee
ebensing <ebensing@ejb-VirtualBox.(none)>
2012-01-16 18:48:23 -0500

    added new script to pull the daily/weekly scripts and editted php page to support it

--- seedScript.php
+++ seedScript.php
@@ -1,6 +1,5 @@
 <?php
 
-exec(`/usr/local/bin/python /usr/home/jedarchi/public_html/production/python/getUnarchivedSeeds.py`);
-$file = file_get_contents('/usr/home/jedarchi/public_html/production/python/missing_seeds.txt');
-echo $file
+echo exec('/usr/local/bin/python /usr/home/jedarchi/public_html/production/python/seedFreq.py');
+
 ?>

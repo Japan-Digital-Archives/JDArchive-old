169bc2a896997f653947cdd8eb1e6a252eafd22a
ebensing <ebensing@ejb-VirtualBox.(none)>
2012-01-16 22:26:17 -0500

    fixes for the seed script display page

--- seedScript.php
+++ seedScript.php
@@ -1,7 +1,7 @@
 <?php
 
 $arr = array();
-exec('/usr/home/jedarchi/bin/freqScript.sh', $arr);
+exec('/usr/home/jedarchi/bin/freqScript.sh 2>&1', $arr);
 
 foreach($arr as &$val) {
     print html_entity_decode(utf8_decode($val), ENT_NOQUOTES, 'UTF-8');

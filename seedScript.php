92b636c1c245f363487c811102f31dd2f2b8ea20
Arne Brasseur <arne@arnebrasseur.net>
2012-02-17 14:30:25 -0500

    added japanese text to explore

--- seedScript.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$arr = array();
-exec('/usr/home/jedarchi/bin/freqScript.sh 2>&1', $arr);
-
-foreach($arr as &$val) {
-    print html_entity_decode(utf8_decode($val), ENT_NOQUOTES, 'UTF-8');
-}
-?>

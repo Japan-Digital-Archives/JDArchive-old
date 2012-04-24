69664d00f8059bdd612927e25ac8b5446812bc2b
ebensing <ebensing38@gmail.com>
2012-04-23 22:01:53 -0400

    testing pushing to new box

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header("Cache-Control: must-revalidate, max-age=600");
 header("Vary: Accept-Encoding");
 require_once(dirname(__FILE__). '/inc/common.php');
 require_once(dirname(__FILE__). '/inc/templates/head.php');
-
+//test
 //$language = language();
 
 ?>

533f4d3951524be841922c149e2a36cbf2c6d74c
Ubuntu <ubuntu@jdarchive.org>
2012-04-24 02:03:34 +0000

    prod changes

--- index.php
+++ index.php
@@ -1,58 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--[if lt IE 9 ]>    <html xmlns="http://www.w3.org/1999/xhtml" class="ie8">    <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html xmlns="http://www.w3.org/1999/xhtml"><!--<![endif]-->
 <?php
 header("Cache-Control: must-revalidate, max-age=600");
 header("Vary: Accept-Encoding");
-require_once(dirname(__FILE__). '/inc/common.php');
-require_once(dirname(__FILE__). '/inc/templates/head.php');
-
 ?>
-<link href="/lib/launch.css" rel="stylesheet" type="text/css" media="screen" />
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Digital Archive of Japan's 2011 Disasters</title>
+<meta name="keywords" content="digital archive japan 2011 earthquake tsunami aftermath reischauer institute harvard" />
+<meta name="description" content="Digital Archive of the Japan 2011 Earthquake and Aftermath" />
+<link href="lib/landing.css" rel="stylesheet" type="text/css" media="screen" />
 <script src="http://code.jquery.com/jquery-latest.js"></script>
+<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
+<script charset="utf-8" src="lib/landing.js" type="text/javascript"></script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22729281-2']);
+  _gaq.push(['_trackPageview']);
 
-<div id="outerbox">
-<table id="links">
-<tr>
-<td>
-	<div id="homelogoboxjp" onClick="initJp();">
-		<div class="logolarge">
-	   		<img class="japaneselogo" src="lib/images/logo_large_ja.png">
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+</head>
+
+<body>
+<div id="header-bar">
+	<header>
+		<div id="title">
+			<img src="lib/images/header-jp.png">
 		</div>
+		<div id="language">
+			<span id="lang">English</span>
+		</div>
+		
+	</header>
+</div>
+
+
+<div id="content">
+
+<div id="middle">
+	<div id="register-header">試作モデル αlfa（要登録）:</div>
+	
+	<div id="prototype">
+		<img class="prototype-ss" src="lib/images/prototype_ss2.png">
 	</div>
-</td>
-<td id="explore">
-	<a href="../explore/?la=ja">
-		<div class="homebuttonblack">
-	 		<p class="japanesetitle txtwhite">現在の<br/>アーカイブ</p>
-	 	</div>
-	</a>
-</td>
-</tr>
-<tr>
-<td>
-	<div id="homelogoboxen" onClick="initEn();">
-		<div class="logolarge">
-	   		<img class="englishlogo" src="lib/images/logo_large_w.png">
-		</div>
+
+	<div id="archi-links">
+	<ul>
+		<li id="l1"><a href="../explore/?la=ja">
+			<div class="btn shadow archi">このアーカイブについて</div>
+		</a></li>
+		<li id="l2"><a href="../seeds/?la=ja">
+			<div class="btn shadow archi">ウェブ・アーカイブ検索</div>
+		</a></li>
+		<li id="l3"><a href="http://worldmap.harvard.edu/japanmap/">
+			<div class="btn shadow archi">震災情報レイヤー地図</div>
+		</a></li>
+		<li id="l4"><a href="../featured/?la=ja">
+			<div class="btn shadow archi">わたしの「東日本大震災」</div>
+		</a></li>
+	</ul>
 	</div>
-</td>
-<td id="new" style="padding-bottom: 25px">
-	<a href="../new/?la=ja">
-		<div class="homebuttonwhite">
-			<p class="japanesetitle txtgrey">これからの<br />アーカイブ</p>
+</div>
+
+<div id="bottom-bar">
+<div id="register">
+	<form name="register-form" id="register-form" action="" method="POST">  
+		<div class="fields">
+			<div class="fieldcontainer">
+				<div class="label">
+			    	<label for="name" id="name" >お名前</label> 
+				</div> 
+				<input type="text" name="name" id="home-name-text-field" class="large" size="30" value="">
+			</div>
+			<div class="fieldcontainer" style="padding-top:15px;">
+				<div class="label">
+					<label for="email" id="email">Eメール</label>  
+				</div>
+				<input type="text" name="email" id="home-email-text-field" class="large" size="30" value="">
+				<input type="hidden" name="language" id="l" value="日本語">
+			</div>
+			<div id="results" class="fieldcontainer" style="padding-top:15px;"></div>
 		</div>
-	</a>
-</td>
-</tr>
-</table>
-<script charset="utf-8" src="/lib/launch.js" type="text/javascript"></script>
+		<input type="submit" name="register" id="register-button" class="btn rounded bluegrad" value="新規登録"> 
+	</form>
+</div>
+
+<div id="logo">
+	<img class="japaneselogo" src="lib/images/logo_large_ja.png">
+</div>
 
 
 </div>
-<div id="footer-bgcontent">
-  <div id="footer">
-    <div class="footerleft">
-    <a href="http://twitter.com/#!/jdarchive"><img style="padding-top:4px" src="/lib/images/t.png"></a> <br />
-    <a href="https://www.facebook.com/pages/Digital-Archive-of-Japans-2011-Disasters/143040332434759"><img src="/lib/images/f.png"></a>
-    </div>
-    <p><a href="http://www.fas.harvard.edu/~rijs/"><img src="/lib/images/reisch_logo.png" style="margin-top:2px; border:none;" /></a></p>
+
 </div>
 
+</body>
+</html>

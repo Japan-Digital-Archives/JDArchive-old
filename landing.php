f10a99d4d800209f62ab521d87b6ef74737b3222
Julie Zhang <blackdragon7170@gmail.com>
2012-05-02 14:14:55 -0400

    cleaned up landing

--- landing.php
+++ landing.php
@@ -1,21 +1,8 @@
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--[if lt IE 9 ]>    <html xmlns="http://www.w3.org/1999/xhtml" class="ie8">    <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html xmlns="http://www.w3.org/1999/xhtml"><!--<![endif]-->
 <?php
 header("Cache-Control: must-revalidate, max-age=600");
 header("Vary: Accept-Encoding");
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Digital Archive of Japan's 2011 Disasters</title>
-<meta name="keywords" content="digital archive japan 2011 earthquake tsunami aftermath reischauer institute harvard" />
-<meta name="description" content="Digital Archive of the Japan 2011 Earthquake and Aftermath" />
-<link href="lib/landing.css" rel="stylesheet" type="text/css" media="screen" />
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
-<script charset="utf-8" src="lib/landing.js" type="text/javascript"></script>
 
 <?php
 
